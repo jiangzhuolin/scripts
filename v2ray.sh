42edb835d2b8bf6f8c7f1b37bb1b21573f996056
jiangzhuolin <jiangzl975@gmail.com>
2022-03-03 12:54:43 +0800

    Update v2ray.sh
    update code to resolve domain to ip

--- v2ray.sh
+++ v2ray.sh
@@ -272,7 +272,8 @@ getData() {
             CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
             KEY_FILE="/etc/v2ray/${DOMAIN}.key"
         else
-            resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
+            # resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
+	    resolve=`nslookup ${DOMAIN}`
             res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
